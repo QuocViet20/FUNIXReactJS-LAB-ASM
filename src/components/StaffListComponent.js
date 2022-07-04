a61a322e1cf8f4443ecc85f96020f0d103eac06b
QuocViet20 <luongquocvietbk58@gmail.com>
2022-07-04 20:59:45 +0900

    first update

---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import dateFormat, { masks } from "dateformat";
+import { Card, CardTitle } from "reactstrap";
+
+class StaffList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedStaff: null,
+    };
+  }
+  onDishSelect(staff) {
+    this.setState({ selectedStaff: staff });
+  }
+
+  renderDish(staff) {
+    if (staff != null)
+      return (
+        <Card body>
+          <CardTitle tag="h5">{staff.name}</CardTitle>
+          <CardTitle>
+            Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}
+          </CardTitle>
+          <CardTitle>
+            Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}
+          </CardTitle>
+          <CardTitle>Phòng ban: {staff.department.name}</CardTitle>
+          <CardTitle>Số ngày nghỉ còn lại: {staff.annualLeave}</CardTitle>
+          <CardTitle>Số ngày làm thêm: {staff.overTime}</CardTitle>
+        </Card>
+      );
+    else
+      return (
+        <div>
+          <CardText tag="h5">Bấm vào tên nhân viên để xem thông tin</CardText>
+        </div>
+      );
+  }
+
+  render() {
+    const menu = this.props.staffs.map((staff) => {
+      return (
+        <div className="col-12 col-md-3 col-sm-5 m-2 ">
+          <Card key={staff.id} onClick={() => this.onDishSelect(staff)}>
+            <CardTitle className="px-1">{staff.name}</CardTitle>
+          </Card>
+        </div>
+      );
+    });
+
+    return (
+      <div className="container my-1">
+        <div className="row">{menu}</div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            {this.renderDish(this.state.selectedStaff)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default StaffList;
